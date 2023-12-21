e88cbaded1e681584630dfcaa177b3a6979de752
Zach Hawtof <zhawtof@gmail.com>
2023-12-21 15:35:53 -0500

    Initial Zach

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,22 @@
+import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Tightknit Docs</span>,
   docsRepositoryBase: "https://github.com/TightknitAI/tightknit-documentation",
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Tightknit",
+      };
+    }
+  },
+
   footer: {
     text: "Tightknit",
   },
+  primarySaturation: 50,
 };
 
 export default config;
