287770dd798f286a34f666faa6d6ae6a2a72713e
Stephen <StephenTangCook@gmail.com>
2024-05-15 13:37:37 -0700

    add top-level buttons and tailwindcss

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,6 @@
+import { FileCode, LibraryBig } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
@@ -20,6 +22,7 @@ const config: DocsThemeConfig = {
       <Image src="/slack-logo.svg" width={30} height={30} alt="Slack logo" />
     ),
   },
+
   head: (
     <>
       <title>Tightknit Docs</title>
@@ -41,7 +44,42 @@ const config: DocsThemeConfig = {
     text: "Copyright © 2024 Tightknit, Inc.",
   },
   sidebar: {
+    defaultMenuCollapseLevel: 1,
     toggleButton: true,
+    titleComponent: ({ type, title, route }) => {
+      const { asPath } = useRouter();
+      if (type === "separator" && title === "Switcher") {
+        return (
+          <div className="-mx-2 hidden md:block">
+            {[
+              { title: "Docs", path: "/basics", Icon: LibraryBig },
+              { title: "Guides", path: "/events", Icon: FileCode },
+            ].map((item) => {
+              // console.log(item);
+              return asPath.startsWith(item.path) ? (
+                <div
+                  key={item.path}
+                  className="group mb-3 flex flex-row items-center gap-3 nx-text-primary-800 dark:nx-text-primary-600"
+                >
+                  <item.Icon className="w-7 h-7 p-1 border rounded nx-bg-primary-100 dark:nx-bg-primary-400/10" />
+                  {item.title}
+                </div>
+              ) : (
+                <Link
+                  href={item.path}
+                  key={item.path}
+                  className="group mb-3 flex flex-row items-center gap-3 text-gray-500 hover:text-primary/100"
+                >
+                  <item.Icon className="w-7 h-7 p-1 border rounded group-hover:bg-border/30" />
+                  {item.title}
+                </Link>
+              );
+            })}
+          </div>
+        );
+      }
+      return title;
+    },
   },
   docsRepositoryBase:
     "https://github.com/TightknitAI/tightknit-documentation/tree/main/",
